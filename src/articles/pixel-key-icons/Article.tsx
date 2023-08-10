46a00e4a8bc25ed6e4d5099d467f7f1daf30425e
unknown <josephdong1998@gmail.com>
2023-08-10 13:51:24 -0400

    need to proofread pixel-key-icons article, otherwise its pretty done

--- src/articles/pixel-key-icons/Article.tsx
+++ src/articles/pixel-key-icons/Article.tsx
@@ -7,6 +7,11 @@ import {
 import KeyIcon from '../../parts/KeyIcon.tsx';
 import KeyIconString from '../../parts/KeyIconString.tsx';
 import illustration1 from './assets/illustrations-for-pixel-key-icons-large1.png';
+import illustration2 from './assets/illustrations-for-pixel-key-icons-large2.png';
+import illustration3 from './assets/illustrations-for-pixel-key-icons-large3.png';
+import illustration4 from './assets/illustrations-for-pixel-key-icons-large4.png';
+import illustration5 from './assets/illustrations-for-pixel-key-icons-large5.png';
+
 import { Box, makeStyles } from '@material-ui/core';
 import clsx from 'clsx';
 
@@ -16,11 +21,17 @@ const useStyles = makeStyles(() => (
             display: 'flex',
             justifyContent: 'center',
             marginTop: '40px',
-            marginBottom: '40px'
+            marginBottom: '40px',
+            flexWrap: 'wrap'
         },
 
-        illustrations: {
-            height: '512px'
+        illustrationsLarge: {
+            height: '512px',
+        },
+
+        illustrationsComparison: {
+            height: '300px',
+            margin: '5px'
         }
     }
 ));
@@ -84,7 +95,7 @@ const Article = () => {
         </ty.ArticleText>
 
         <Box component="div" className={clsx(c.center)}>
-            <Box><img className={clsx(c.illustrations)} src={illustration1}></img></Box>
+            <Box><img className={clsx(c.illustrationsLarge)} src={illustration1}></img></Box>
         </Box>
 
         <ty.ArticleText gutter slightlySmaller>
@@ -108,6 +119,61 @@ const Article = () => {
 
             <KeyIconString content="abcdefghijklmnopqrstuvwxyz" size="64px"/>
 
+        </ty.ArticleText>
+
+        <ty.ArticleSection gutter>
+            The EF Dilemma
+        </ty.ArticleSection>
+
+        <ty.ArticleText slightlySmaller>
+            In general, the design of the characters, A, B, C, D are without incident, but there is a slight problem when it comes to E, and F. Which is that the center horizontal line of E, cannot be placed exactly centrally, equal distance from the top and bottom of the character, it must be placed either closer to the top or closer to the bottom, because the height of the character is 8px, which is an even number.
+        </ty.ArticleText>
+
+        <Box component="div" className={clsx(c.center)}>
+            <img src={illustration2} className={clsx(c.illustrationsComparison)}/>
+            <img src={illustration3} className={clsx(c.illustrationsComparison)}/>
+        </Box>
+
+        <ty.ArticleText slightlySmaller gutter>
+            However, as it is, with all things with limits, there must be a compromise, if we wanted to place the central horizontal line of the E right in the center, we would have to double the resolution of our character set to 32x32 pixels, but that was not part of the plan. But the solution is simple, do both, and decide which one is better, so seen above, there is the E with the central horizontal line closer to the top, and the E with the central horizontal line closer to the bottom. And according to my eyes, the E with the central line closer to the top looks more natrual, so that was selected for the final design.
+
+            <br/>
+            <br/>
+
+            And so this became a commonly used princlple, all of the characters with some sort of central horizonal line are A, B, E, F, G, H, R, S.
+
+            <br/>
+            <br/>
+
+            <KeyIconString content="abefghrs" size='64px'/>
+
+            <br/>
+            <br/>
+
+            Of those, B, E, F, H, R, S, prefer a central horizontal line closer to the top. And A, and G prefer a central horizontal line closer to the bottom.
+        </ty.ArticleText>
+
+        <ty.ArticleSection gutter>Multiple Problems With I</ty.ArticleSection>
+
+        <ty.ArticleText gutter slightlySmaller>
+
+            If you look at the design or almost all I characters, you will see two horizontal lines at the top and bottom of the character, and a pillar, straight down the center connecting the top and bottom. There is almost no getting away from this reality. Now there is always a problem with symmetry and even pixel resolutions in pixel-art, but there is a possible solution which may work sometimes, which is to double the thickness in the center.
+        </ty.ArticleText>
+
+        <Box component="div" className={clsx(c.center)}>
+
+            <img src={illustration4} className={clsx(c.illustrationsComparison)}/>
+            <img src={illustration5} className={clsx(c.illustrationsComparison)}/>
+        </Box>
+
+        <ty.ArticleText gutter slightlySmaller>
+
+            Now here we come to a crossroads, because if I must say so, for the character I in isolation, it seems that to double the thickness of the central pillar seems to be the better choice, as it simply looks good, and it maintains the overall central symmetry, however in the full context, if we decide to go with this solution, it does have some knock-on effects. So say that I has a double thickness central pillar, then should other characters with a central pillar also have it be double thickness. This would affect J, T, Y, but then with the double thick central pillars, those characters begin to stand out, and the overall character set starts to look half regular, and half bold. And what about M and W, do those characters need a double thick central pillar, and if central pillars are double thick, then do side pillars like on E, F, L also need to be double thick? So in my judgement, it seemed that the double thickness solution so solving the symmetry issue of I was not good.
+
+            <br/>
+            <br/>
+
+            The issue with the symmetry of I first arose because the width of all characters up to this point was even, however, if it were allowed that characters could have odd widths, then it would also solve the issue. However, if a character had an odd width, then it would not be possible to center the character in the middle of the icon. So it was decided, mostly arbitrarily, that for characters that needed central vertial symmetry, that they would have an odd width, and the character would be offset 1 pixel to the right, instead of the left, just to maintain consistency accross all icons.
         </ty.ArticleText>
     </Bass>;
 };
