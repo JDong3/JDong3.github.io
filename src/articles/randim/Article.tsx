e2aa308f999b3c0d63301970b59af0ab575555ae
unknown <josephdong1998@gmail.com>
2023-08-22 16:01:04 -0400

    working on writeup for randim, need to figureout the import error/assets includes config

--- src/articles/randim/Article.tsx
+++ src/articles/randim/Article.tsx
@@ -1,159 +1,90 @@
 import Bass from '../Bass.jsx';
-import {
-    Text,
-    Hr,
-    ty,
-} from '../../parts/index.ts';
 
-const Speculation = () => (
-    <Bass>
+import ty from '../../parts/Typographyd.jsx';
+import clsx from 'clsx';
+
+import { Box, makeStyles } from '@material-ui/core';
+import black from './assets/black.bmp';
+import random from './assets/random.bmp';
+import randomgreyscale from './assets/randomgreyscale.bmp';
+
+const useStyles = makeStyles(() => (
+    {
+        center: {
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '40px',
+            marginBottom: '40px',
+            flexWrap: 'wrap'
+        },
+
+        illustrationsLarge: {
+            height: '512px',
+        },
+
+        illustrationsComparison: {
+            height: '300px',
+            margin: '5px'
+        }
+    }
+));
+
+const Speculation = () => {
+    const c = useStyles();
+    return <Bass>
 
         <ty.Title smaller serif>
-            Scientific Theories as Truth
+            Random Image
         </ty.Title>
 
         <ty.ArticleSection gutter>
-            Motivation
+            Description
         </ty.ArticleSection>
 
         <ty.ArticleText gutter slightlySmaller>
 
-            There’s no single thing that we can call truth, but instead, truth is more of a collection of separate but related things. In fact, this is actually case for most things. For example, lets take a look at a computer. A computer is a common office, and household electronic device that’s used for work, and recreation. But, it’s also a collection of memory, computation units, hard disk, etc. We don’t say that one of these definitions of a computer is wrong and the other is right, because that just doesn’t make sense in this context. Instead we can interpret these definitions as encapsulating different aspects of the computer, one of them being the components of the computer, and the other being the uses of a computer.
+            Random Image (randim) is a python3 based command line tool that allows the user to generate RNG based images with a few options for configuration.
 
             <br/>
-            <br/>
 
-            What I wan’t to talk about today is a specific kind of idea which I’m going to call “scientific theories”. For our purposes, a scientific theory is any idea that proposes an explanation for the mechanics behind some phenomena that we might experience, so this is a bit more broad that what scientists today might call a scientific theory. What we’re concerned with today is the peculiar behaviours of these scientific theories, where they come from, and what is their purpose.
+            <b>Project Link:</b> <a href="https://github.com/JDong3/randim" target="_blank">https://github.com/JDong3/randim</a>
 
         </ty.ArticleText>
 
         <ty.ArticleSection gutter>
-            The Reality vs Perception
+            Examples
         </ty.ArticleSection>
 
         <ty.ArticleText gutter slightlySmaller>
-            Do we “know” what reality “is”? Well actually no, because if we’re both looking at a tree, in most circumstances its pretty safe to say that we both see a tree. But we don’t actually see the same thing. Because we have we have different eyes, and minds, so is seems reasonable to think that both the raw data going into our sensors, and the interpretation of that data, would be different. So there is no reason to believe that we should see the same image of the tree, or feel the same emotions by looking at the tree.
-
+            A black image with 400x800 pixels resolution (native resolution), using the seed 'asdf', output to 'black.bmp'.
             <br/>
-            <br/>
-
-            This suggests that what we experience is not the reality, but a modified reality that is influenced by ourselves and our culture. From here on out, for non-confusion purposes, I’m going to refer to what we understand to be the real world, as reality, and what we experience, as our perception.
-
-            <br/>
-            <br/>
-
-            What we normally think of as reality is actually not the real thing, but our perception. So in a sense our notions and beliefs are not based in some absolute reality, but they are necessarily based in our perception which is influenced by ourselves and our culture. (record here)
+            <code>python randim.py --type black --width 400 --height 800 --seed asdf --output black.bmp</code>
         </ty.ArticleText>
 
-        <ty.ArticleSection gutter>
-            The Map Territory Relationship
-        </ty.ArticleSection>
+        <Box component="div" className={clsx(c.center)}>
+            <Box><img className={clsx(c.illustrationsLarge)} src={black}></img></Box>
+        </Box>
 
-        <ty.ArticleText gutter slightlySmaller>
-            The map is not the territory, just by virtue of its size its much smaller than the territory, so you can’t expect all of details of the territory to be contained in the map. And If there were a map that contained all of the details of the territory, it would necessarily just be the territory itself.
-
+        <ty.ArticleText gutter>
+            A 'random' image, where each pixel has an RGB value chosen pseudo randomly, with a 654x456 pixel native resolution, using the seed 'asdf', output to 'random.bmp'.
             <br/>
-            <br/>
-
-            Is this a hydrogen? Well no, its a diagram, but its also a representation of hydrogen. In school this is how were taught to think about hydrogen, because if we do, we can make predictions, like how heavy is it, what it might combine with?
-
-            <br/>
-            <br/>
-
-            Representations are analogous to maps, not maps of territory, but maps of reality. We suspect that these representations are like reality, but we can’t expect them to be reality, because at the end of the day its our percreption, and not reality that we’ve created these representations from.
-
+            <code>python randim.py --type random --width 654 --height 456 --seed asdf --output random.bmp</code>
         </ty.ArticleText>
 
-        <ty.ArticleSection gutter>
-            Old and New Scientific Theories
-        </ty.ArticleSection>
+        <Box component="div" className={clsx(c.center)}>
+            <Box><img className={clsx(c.illustrationsLarge)} src={random}></img></Box>
+        </Box>
 
-        <ty.ArticleText gutter slightlySmaller>
-            I want to examinine two different “scientific theories” for why things fall. According to the Greeks, the world is made of four elements, earth, air, fire, and water. Now each of these elements have a natural place, like earth is beneath us, and air is above us. However upon being disturbed, these elements wish to return to their natural places, so if you throw a rock upwards, it falls back down because the rock is made of earth, and the earth wishes to return to its natural place.
-
-            <br/>
-            <br/>
-
-            But here’s the thing, what is the meaning of natural place? Why do rocks wish to go back to their natural place? And do rocks really wish for anything? Aren’t these just magical notions that don’t reflect reality.
-
-            <br/>
-            <br/>
-
-            So what we say nowadays is that its Newton’s equations of gravity tells us the real reason for why things fall. And this gravity also happens to be the same reason for why the planets go round the sun, and why the moon goes round the earth. Gravity is a force that is felt by a pair of objects in a direction that pulls them together, and it is dependent on the masses of the two objects, and how nea r they are to each other.
-
-            <br/>
-            <br/>
-
-            But here’s the thing, why do we say that Newton is right, and the Greeks are wrong? Well thats because unlike the Greek’s theory, Newton’s theory is true, which is half true. Because the greek theories are also true, in that they also correspond to reality, since after all the Greek’s predict that rocks fall down, and they do.
-
-            <br/>
-            <br/>
-
-            The difference is that Newton’s theory make sense for us, in our modern context, because in our modern world we happen to require more from our scientific theories than that Greek theory can provide. But this does not mean that Newton’s theory is absolutely true, because first off we actually know that Newton’s theory doesn’t actually correspond to reality as Einstein developed a more powerful theory of general relativity.
-
-            <br/>
-            <br/>
-
-            But not only that, Newton’s theory actually contains the same magical notions that the Greek’s theory did. Because consider what it means for “gravity” to be a force. What causes the direction of that force, why is it dependent on distance and mass. What is mass, and why do forces make objects move.
-
-            <br/>
-            <br/>
-
-            Now this isn’t to say that we shouldn’t use Newton’s theories because they contain magical notions, because our scientific theories will always contain magical notions. As the old magical notions are replaced with more fundamental parts, the structure of those more fundamental parts become the new magical notions, so its magical notions all the way down. But it doesn’t matter because they work, which is exactly what we need from our scientific theories.
+        <ty.ArticleText gutter>
+            A 'random greyscale' image, like random, but the image can only contain grey pixels, with native resolution 1920x1080, using the seed 'asdf', and output to 'randomgreyscale.bmp'.
         </ty.ArticleText>
 
-        <ty.ArticleSection gutter>
-            On the Nature of Logic
-        </ty.ArticleSection>
+        <Box component="div" className={clsx(c.center)}>
+            <Box><img className={clsx(c.illustrationsLarge)} src={randomgreyscale}></img></Box>
+        </Box>
 
-        <ty.ArticleText gutter slightlySmaller>
-            Our pride of modern scientific thinking is that our theories don’t rely on any magical notions, but instead on logic, and reason alone. But what is logic?
+    </Bass>;
 
-            <br/>
-            <br/>
-
-            These are the rules of logic. There are other rules and extensions of logic, but all of propositioal, and first order logic can be derived from just these rules here. These tables govern the behaviour of creating compound statements with the “not”, “and”, and “or” operators respectively. We can take a look at one of the rules to get an idea for what the tables mean. For example we can take a look at this row in the “or” table.
-
-            <br/>
-            <br/>
-
-            This row tells us what type of statement we’ll get if we combine A a true statement, and B and false statement with the “or” operator. Lets say that A is something true, like the sky is blue, and B is something false like 1 + 1 = 3. Now the combined statement, the sky is blue or 1 + 1 = 3, is true.
-
-            <br/>
-            <br/>
-
-            Now all of this seems reasonable, but there’s always the question of where do these rules come from. The answer being that they don’t. We call the rules of logic axiomatic because they are not justified, because they can’t be justified.
-
-            <br/>
-            <br/>
-
-            The thing about logic is that logic itself is also a scientific theory, whereby the rules have been crafted by our experience and languages, in order to categorize, and combine other things that we know about. However, like other scientic theories, logic is necessarily also magical.
-
-            <br/>
-            <br/>
-
-            Magic, like truth, is composed of separate but related aspects. One aspect of magic is the showmanship, and wonder that you would see at a magic show. But another aspect of magic is the unexplained, or the mystery, which is exactly what we are seeing from logic.
-
-            <br/>
-            <br/>
-
-            I want to reiterate that saying logic is magical is not me telling you to not use logic, because we are not concerned about whether logic is magical or not, only that it works.
-        </ty.ArticleText>
-
-        <ty.ArticleSection gutter>
-            Conclusions?
-        </ty.ArticleSection>
-
-        <ty.ArticleText gutter slightlySmaller>
-            Its a truism that all roads lead to Darwinism. Our idea of usefulness is related to the things that we wan’t. And at least in theory, the things that we desire, should be related to things that increase our chances of survival. And scientific theories seem to be things that increase our chances of getting the things that we want. For example, if we want to launch a satellite into orbit, we need to have good scientific theories on the behaviour of rockets, orbital trajectories, composite materials, etc.
-
-            <br/>
-            <br/>
-
-            And in this context it becomes clear that there is no pressure in the systems of scientific theory formation that attempts to remove magical thinking, if that’s even possible. But the main pressure is just to come up with theories that work well enough so that we can do whatever it is that we want to do.
-        </ty.ArticleText>
-
-    </Bass>
-);
+};
 
 export default Speculation;
